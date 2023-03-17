a94542a0f6cdb9e0f4f2093b5d74ac8c68c0117d
Hossam <hossamfathi050@outlook.com>
2023-03-17 13:44:31 +0300

    Ready

--- training/src/project/settings.py
+++ training/src/project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-2ry5xgwma#6&k^_ad#ulyf+3tq%()xx+$trn(jl$qcy37-dor4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['trainingok.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -117,12 +117,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static" , "static_root")
