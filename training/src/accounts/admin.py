06637a912f2b61e2019eaf641e41d25f4fab7fc7
Hossam <hossamfathi050@outlook.com>
2023-03-17 09:59:13 +0300

    add app accounts

--- training/src/accounts/admin.py
+++ training/src/accounts/admin.py
@@ -1,3 +1,13 @@
+
 from django.contrib import admin
 
 # Register your models here.
+from .models import Profile,User
+from import_export.admin import ImportExportModelAdmin
+
+
+
+@admin.register(Profile)
+class ProfileImportExport(ImportExportModelAdmin):
+    pass
+
--- training/src/accounts/models.py
+++ training/src/accounts/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+
+
+Style_Traning=[
+    ('تنافسي','تنافسي'),
+    ('تشاركي','تشاركي'),
+    ]
+
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, related_name='user_profile', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile/'  , blank=True, null=True)
+    phone_number = models.CharField(max_length=16 , blank=True, null=True)
+    school = models.CharField(_("مركز العمل"),max_length=50 , blank=True, null=True)
+    style_traning=models.CharField(_("نمط الحشد"),max_length=50,choices= Style_Traning)
+     
+    def __str__(self):
+        return str(self.user)
+
+    
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+            
